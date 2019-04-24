534a88bcf386c1a7e15fececb4ee9fda37e65d50
fiishe <yoon512@gmail.com>
2019-04-24 08:02:08 -0400

    fix tests

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -4,7 +4,8 @@ FactoryBot.define do
   factory :user do
     sequence(:snowflake) {|n| "%012d" % [n]}
     username { "userguy" }
-    sequence(:discrim) {|n| "%04d" % [n]}
+    sequence(:discriminator) {|n| "%04d" % [n]}
     nick { "The Guy" }
+    access_token { SecureRandom.urlsafe_base64 }
   end
 end
