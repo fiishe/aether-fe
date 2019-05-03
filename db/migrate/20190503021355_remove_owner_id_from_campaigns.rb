c397057d1ce8dfe7bc26048f7846adb6be42aace
fiishe <yoon512@gmail.com>
2019-05-02 22:59:08 -0400

    remove breaking presence constraint

--- db/migrate/20190503021355_remove_owner_id_from_campaigns.rb
+++ db/migrate/20190503021355_remove_owner_id_from_campaigns.rb
@@ -1,5 +1,5 @@
 class RemoveOwnerIdFromCampaigns < ActiveRecord::Migration[5.2]
   def change
-    remove_column :campaigns, :owner_id, :integer, null: false
+    remove_column :campaigns, :owner_id, :integer
   end
 end
