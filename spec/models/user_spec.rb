792dae5026ac78f08f5c3d9758f35374b30acb25
fiishe <yoon512@gmail.com>
2019-05-02 22:18:18 -0400

    user validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,29 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   subject { FactoryBot.create(:user) }
 
-  describe "A User created by FactoryBot (see spec/support/factory_bot.rb)" do
+  describe "A User created by FactoryBot (spec/support/factory_bot.rb)" do
     it "is valid with valid attributes" do
       expect(subject).to be_valid
+    end
+
+    it "is invalid with a discriminator of the wrong length" do
+      subject.discriminator = "123"
+      expect(subject).to_not be_valid
+    end
+
+    it "is valid with no nickname" do
+      subject.nick = nil
+      expect(subject).to be_valid
+    end
+
+    it "is invalid with a nick that is too short" do
+      subject.nick = "a"
+      expect(subject).to_not be_valid
+    end
+
+    it "is invalid with a nick that is too long" do
+      subject.nick = "0123456789abcdefggggg oof oof oof oof oof"
+      expect(subject).to_not be_valid
     end
   end
 end
