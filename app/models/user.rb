fbe8f2d7deb3fd052b9306856a5f3677053c89ab
fiishe <yoon512@gmail.com>
2019-05-03 22:58:35 -0400

    add method to get owned campaigns

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
 
   validates :discriminator, length: { is: 4 }
   validates :nick, length: { minimum: 2, maximum: 16 }, allow_nil: true
+
+  def owned_campaigns
+    # includes(:campaigns) eager-loads the campaigns model to reduce queries
+    self.campaign_memberships.where(role: "owner").includes(:campaign).map do |campaign_membership|
+      campaign_membership.campaign
+    end
+  end
 end
