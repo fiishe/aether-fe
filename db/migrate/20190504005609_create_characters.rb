54cead31db7abadd494d4ce9c706fe2bcd785360
fiishe <yoon512@gmail.com>
2019-05-03 22:39:09 -0400

    add character model

---
+++ db/migrate/20190504005609_create_characters.rb
@@ -0,0 +1,13 @@
+class CreateCharacters < ActiveRecord::Migration[5.2]
+  def change
+    create_table :characters do |t|
+      t.string :name, null: false
+      t.integer :level, null: false, default: 1
+      t.boolean :promoted, null: false, default: false
+
+      t.integer :user_id, null: false
+      t.integer :campaign_id
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_03_021355) do
+ActiveRecord::Schema.define(version: 2019_05_04_005609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,16 @@ ActiveRecord::Schema.define(version: 2019_05_03_021355) do
 
   create_table "campaigns", force: :cascade do |t|
     t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "characters", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "level", default: 1, null: false
+    t.boolean "promoted", default: false, null: false
+    t.integer "user_id", null: false
+    t.integer "campaign_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
