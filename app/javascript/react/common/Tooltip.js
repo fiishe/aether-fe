666212c1231011ad90bf980bfb22fd549d0ff296
fiishe <yoon512@gmail.com>
2020-06-18 22:50:53 +0900

    implement tooltips in react

--- app/javascript/react/common/Tooltip.js
+++ app/javascript/react/common/Tooltip.js
@@ -1,25 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Tooltip = props => {
-  /*
-  return(
-    <span data-tooltip
-      tabindex="1"
-      aria-haspopup="true"
-      className="has-tip"
-      title={props.body}
-      >
-      <i className="fas fa-question-circle tooltip-icon" />
-    </span>
-  )
-  */
-  return(
-    <i className="fas fa-question-circle tooltip-icon has-tip"
-      data-tooltip
-      aria-haspopup="true"
-      title={props.body}
-      />
-  )
+class Tooltip extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isOpen: false
+    }
+    this.tipRef = React.createRef()
+    this.tip = null
+
+    this.open = this.open.bind(this)
+    this.close = this.close.bind(this)
+    this.toggle = this.toggle.bind(this)
+  }
+
+  open() {
+    this.setState({ isOpen: true })
+  }
+
+  close() {
+    this.setState({ isOpen: false })
+  }
+
+  toggle() {
+    this.setState({ isOpen: !this.state.isOpen })
+  }
+
+  render() {
+    return(
+      <span className="tooltip-container"
+        onClick={this.toggle}
+        onMouseEnter={this.open}
+        onMouseLeave={this.close}
+        >
+        <i className="fas fa-question-circle"
+          title={this.props.body}
+          />
+        <div className={this.state.isOpen ? '' : 'display-none'}>
+          {this.props.body}
+        </div>
+      </span>
+    )
+  }
 }
 
 export default Tooltip
