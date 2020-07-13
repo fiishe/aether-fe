de21c2ca25bd01179e33178595a3fd3a90874f64
fiishe <yoon512@gmail.com>
2020-07-13 20:34:42 +0900

    CampaignIndex skeleton + campaigns redux module

---
+++ app/javascript/react/campaigns/CampaignIndex.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+import { connect } from 'react-redux'
+import { campaigns } from '../redux/modules/campaigns'
+
+class UserCharactersContainer extends FetchingComponent {
+  constructor(props) {
+    super(props)
+  }
+}
+
+export default UserCharactersContainer
---
+++ app/javascript/react/redux/modules/campaigns.js
@@ -0,0 +1,22 @@
+import makeActionCreator from '../utils/makeActionCreator'
+import { fetchGet } from "../../lib/defaultFetch"
+
+// INITIAL STATE
+const initialState = {
+
+}
+
+// ACTION CREATORS
+
+// REDUCER
+const campaigns = (state = initialState, action) => {
+  switch(action.type) {
+
+    default:
+      return state
+  }
+}
+
+export {
+  campaigns
+}
