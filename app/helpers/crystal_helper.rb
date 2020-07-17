20b9b561150ec7f1269261f1925fbc53110fcc97
fiishe <yoon512@gmail.com>
2020-07-17 22:28:29 +0900

    create helper for generating crystal IDs

--- app/helpers/crystal_helper.rb
+++ app/helpers/crystal_helper.rb
@@ -1,11 +1,11 @@
 module CrystalHelper
   EPOCH = Time.at(ENV['EPOCH'].to_i)
   BIT_SHIFT = 12
-  INCREMENT_PERIOD = 1 << 12 # = 2^12 = 4096
+  INCREMENT_PERIOD = 1 << BIT_SHIFT # = 2^12 = 4096
 
   def generate_crystal(increment)
     # get current time (ms)
-    time = get_time()
+    time = get_time_ms()
 
     # shift it to make space for increment
     time = time << BIT_SHIFT
@@ -15,13 +15,20 @@ module CrystalHelper
 
     # the result is a sortable "crystal ID" that can be publicly exposed
     # without revealing information about the app's volume of usage!
+    #
+    # crystal = (ms since EPOCH << 12) + (increment % 4096)
+  end
+
+  def extract_time(crystal)
+    # return approximate time crystal was generated (to seconds)
+    crystal_seconds = (crystal >> BIT_SHIFT) / 1000
+    return EPOCH + crystal_seconds
   end
 
   private
 
-  def get_time
-    # number of milliseconds since EPOCH
-    seconds = (Time.now - EPOCH).to_f
-    return (seconds * 1000.0).to_i
+  def get_time_ms #in ms since EPOCH
+    time_seconds = Time.now - EPOCH
+    return (time_seconds * 1000.0).to_i
   end
 end
--- spec/helpers/crystal_helper_spec.rb
+++ spec/helpers/crystal_helper_spec.rb
@@ -11,12 +11,31 @@ require 'rails_helper'
 #   end
 # end
 
-RSpec.describe CrystalHelper, type: :helper do
-  describe "generate_snowflake" do
-    id = helper.generate_id
+EPOCH = Time.at(ENV['EPOCH'].to_i)
 
-    it "generates a number within 1 second of the time since EPOCH" do
-      binding.pry
+def current_time_ms # in ms
+  (Time.now - EPOCH) * 1000.0
+end
+
+RSpec.describe CrystalHelper, type: :helper do
+  describe "generate_crystal" do
+    it "generates a crystal with an accurate time component (within 1s)" do
+      id = helper.generate_crystal 0
+      id_time_ms = id >> 12 # time since epoch in ms
+
+      dt = (id_time_ms - current_time_ms).to_i
+      expect(dt.abs).to be < 1000
+    end
+  end
+
+  describe "extract_time" do
+    it "returns the time at which the crystal was generated (within 1s)" do
+      start_time = Time.now
+      id = helper.generate_crystal 0
+      id_time = helper.extract_time id  # Time at which crystal was created
+
+      dt = start_time.to_f - id_time.to_f
+      expect(dt.abs).to be < 1
     end
   end
 end
